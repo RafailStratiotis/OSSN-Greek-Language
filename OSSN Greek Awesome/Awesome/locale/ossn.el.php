0a7494dbadba9b26556314524c61c090b8fddbbd
RafailStratiotis <57919282+RafailStratiotis@users.noreply.github.com>
2020-04-04 13:07:55 +0300

    Add files via upload

---
+++ OSSN Greek Awesome/Awesome/locale/ossn.el.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Open Source Social Network
+ *
+ * @packageOpen Source Social Network
+ * @author    Open Social Website Core Team <info@informatikon.com>
+ * @copyright 2014-2016 SOFTLAB24 LIMITED
+ * @license   General Public Licence http://www.opensource-socialnetwork.org/licence
+ * @link      https://www.opensource-socialnetwork.org/
+ */
+$el = array(
+		'admin:theme:awesome' => 'Awesome Ρυθμίσεις',
+		'awesometheme:settings' => 'Awesome Ρυθμίσεις',
+		'awesometheme:primary' => 'Μπλε',
+		'awesometheme:warning' => 'Πορτοκάλι',
+		'awesometheme:info' => 'Μπλε Του Ουρανού',
+		'awesometheme:success' => 'Πράσινο',
+		'awesometheme:danger' => 'Κόκκινο',
+		'awesometheme:black' => 'Μαύρο',
+		'awesometheme:settings:saved' => 'Οι ρυθμίσεις αποθηκεύτηκαν',
+		'awesometheme:settings:error' => 'Δεν είναι δυνατή η αποθήκευση των ρυθμίσεων',
+);
+ossn_register_languages('el', $el);
