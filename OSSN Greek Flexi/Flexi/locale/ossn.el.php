29f732d3798e432a8684d27e27d10551305159b2
RafailStratiotis <57919282+RafailStratiotis@users.noreply.github.com>
2020-04-04 13:07:35 +0300

    Add files via upload

---
+++ OSSN Greek Flexi/Flexi/locale/ossn.el.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Open Source Social Network
+ *
+ * @packageOpen Source Social Network
+ * @author    Open Social Website Core Team <info@informatikon.com>
+ * @copyright 2014-2016 SOFTLAB24 LIMITED
+ * @license   General Public Licence http://www.opensource-socialnetwork.org/licence
+ * @link      https://www.opensource-socialnetwork.org/
+ */
+$el = array(
+	'flexi:latest:members' => 'Τελευταία μέλη',
+	'flexi:latest:friends' => 'Τελευταίοι φίλοι',
+);
+ossn_register_languages('el', $el); 
