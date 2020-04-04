c2d845be0d75496e389051598db8850265386683
RafailStratiotis <57919282+RafailStratiotis@users.noreply.github.com>
2020-04-04 13:02:31 +0300

    Add files via upload

---
+++ OSSN V5.1/components/ExpenseManagement/locale/ossn.el.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Open Source Social Network
+ *
+ * @packageOpen Source Social Network
+ * @author    Open Social Website Core Team <info@informatikon.com>
+ * @copyright 2014 iNFORMATIKON TECHNOLOGIES
+ * @license   General Public Licence http://www.opensource-socialnetwork.org/licence
+ * @link      http://www.opensource-socialnetwork.org/licence
+ */
+$el = array(
+		'expensemanagement' => 'Φύλλο Εξόδων',
+		'em:addexpense' => 'Προσθέστε μια δαπάνη',
+		'em:date' => 'Ημερομηνία',
+		'em:itemname' => 'Ονομα προϊόντος',
+		'em:price' => 'Τιμή',
+		'em:itemslist' => 'Λίστα εξόδων',
+		'em:itemsadded' => 'Τα στοιχεία έχουν προστεθεί στη λίστα',
+		'em:itemsadderror' => 'Τα στοιχεία δεν μπορούν να αποθηκευτούν.',
+		'em:add' => 'Προσθήκη εξόδων για %s/%s',
+		'em:tools' => 'Εργαλεία',
+		'em:total' => 'Σύνολο ',
+		'em:print' => 'Φύλλο εξόδων',
+		'em:dashboard' => 'Πίνακας',
+		'em:months' => 'Μήνες',
+		'em:year' => 'Ετος',
+		'em:list' => 'Λίστα',
+		'em:month:jan' => 'Ιανουάριος',
+		'em:month:feb' => 'Φεβρουάριος',
+		'em:month:mar' => 'Μάρτιος',
+		'em:month:apr' => 'Απρίλιος',
+		'em:month:may' => 'Μάιος',
+		'em:month:jun' => 'Ιούνιος',		
+		'em:month:jul' => 'Ιούλιος',
+		'em:month:aug' => 'Αύγουστος',
+		'em:month:sep' => 'Σεπτέμβριος',
+		'em:month:oct' => 'Οκτώβριος',
+		'em:month:nov' => 'Νοέμβριος',
+		'em:month:dec' => 'Δεκέμβριος',
+		'em:currentmonth' => 'Τρέχων Μήνας',
+		'em:feature:sheets' => 'Δεν μπορείτε να δείτε το μελλοντικό φύλλο αυτή τη στιγμή. Περιμένετε για το χρόνο!',
+);
+ossn_register_languages('el', $el);
